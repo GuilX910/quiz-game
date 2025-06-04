6b5c140adcf5196ad16e1a665136c9fd0afc2c2b
GuilX910 <guilx123321@gmail.com>
2025-06-04 08:30:28 -0300

    update

--- logica.js
+++ logica.js
@@ -1,25 +1,58 @@
 
-botaoIniciar = document.querySelector(".iniciar")
+listaPerguntas = [
+  {
+    pergunta: "Qual o maior ser vivo?",
+    opcoes: [
+      "Baleia Azul",
+      "Fungo de Mel",
+      "Elefante",
+      "Girafa",
+      "Sua mãe"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual a primeira capital do Brasil?",
+    opcoes: [
+      "Taquara",
+      "Brasília",
+      "Salvador",
+      "Rio de Janeiro",
+      "Faixa de gaza"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Em que ano o Brasil ganhou sua segunda Copa do Mundo?",
+    opcoes: [
+      "1974",
+      "1970",
+      "1966",
+      "1962"
+    ],
+    correta: 3
+  }
+]
 
+perguntas = null
+
+botaoIniciar = document.querySelector(".iniciar")
 botaoIniciar.addEventListener("click", iniciarJogo)
 
 function iniciarJogo() {
+  
   telaInicial = document.querySelector(".tela-inicial")
   telaInicial.style.display = "none"
   
   telaPergunta = document.querySelector(".tela-pergunta")
   telaPergunta.style.display = "block"
   
+  mostrarPergunta()
+  
 }
 
-alternativa = document.querySelector(".alternativa")
-alternativa.addEventListener("click", respostaCorreta)
-
-
-function respostaCorreta() {
-  telaPergunta = document.querySelector(".tela-pergunta")
-  telaPergunta.style.display = "none"
+function mostrarPergunta() {
+  pergunta = listaPerguntas[0]
   
-  telaCorreto = document.querySelector(".tela-acertou")
-  telaCorreto.style.display = "block"
+  console.log(pergunta)
 }
